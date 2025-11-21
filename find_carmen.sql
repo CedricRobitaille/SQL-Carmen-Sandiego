94bc118e2b480f56d5fe29ab1b9cc69e58fd9973
Cedric <crubbertire@rogers.com>
2025-11-21 12:51:47 -0500

    Carmen Sandiego - Halfway there! Hot on her trail

--- find_carmen.sql
+++ find_carmen.sql
@@ -55,7 +55,8 @@ San Marino (SMR) Has 1 language
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country.
 --One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-
+SELECT * FROM city WHERE countrycode = 'SMR';
+Cities: Serravalle, San Marino
 
 
 
